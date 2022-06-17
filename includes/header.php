51b2b84745da02ad74c3f088a16df9709c1e96e9
Miguel Cuellar <soporte.ti@jamexico.org.mx>
2022-06-17 00:42:39 -0500

    actualización del header para que logre tomar la ruta principal

--- includes/header.php
+++ includes/header.php
@@ -2,18 +2,19 @@
 <html lang="en">
 <?php //$dir = "http://" . $_SERVER['HTTP_HOST'] . "/eye/"; ?>
 <?php $dir = "https://" . $_SERVER['HTTP_HOST'] . "/"; ?>
+<?php $dir2 = "https://" . "www.jamexicoprimaria.org.mx" . "/"; ?>
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>JA México Primaria</title>
-  <link rel="icon" type="image/ico" href="<?php echo $dir; ?>images/favicon.ico">
-  <link rel="stylesheet" href="<?php echo $dir; ?>css/bootstrap.css">
-  <link rel="stylesheet" href="<?php echo $dir; ?>css/estilos.css">
+  <link rel="icon" type="image/ico" href="<?php echo $dir2; ?>images/favicon.ico">
+  <link rel="stylesheet" href="<?php echo $dir2; ?>css/bootstrap.css">
+  <link rel="stylesheet" href="<?php echo $dir2; ?>css/estilos.css">
   <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400" rel="stylesheet">
-  <link rel="stylesheet" href="<?php echo $dir; ?>css/fontawesome.css">
-  <script src="<?php echo $dir; ?>js/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
-  <script src="<?php echo $dir; ?>js/bootstrap.bundle.js" crossorigin="anonymous"></script>
-  <script src="<?php echo $dir; ?>js/bootstrap.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="<?php echo $dir2; ?>css/fontawesome.css">
+  <script src="<?php echo $dir2; ?>js/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
+  <script src="<?php echo $dir2; ?>js/bootstrap.bundle.js" crossorigin="anonymous"></script>
+  <script src="<?php echo $dir2; ?>js/bootstrap.js" crossorigin="anonymous"></script>
 </head>
 
 <body class="background_gray text-dark-gray align-middle">
